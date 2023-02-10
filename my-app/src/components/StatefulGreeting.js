e74655718942d0f2e1b45d535d7fe9a6229d9d5e
HPCarey <hannahpcarey@gmail.com>
2023-02-10 17:24:34 +0000

    add onlclick functionality

--- my-app/src/components/StatefulGreeting.js
+++ my-app/src/components/StatefulGreeting.js
@@ -10,11 +10,23 @@ class StatefulGreeting extends React.Component {
         }; 
     }
 
+    handleClick() {
+        this.setState({
+          introduction: "Goodbye!",
+          buttonText: 'Enter'
+        }, ()=>{
+            console.log('new state', this.state.introduction);
+            console.log('new state', this.state.buttonText);
+        });
+        console.log(this.state.introduction);
+        console.log(this.state.buttonText);
+      }
+
     render() {
         return (
             <div>
                 <h1>{this.state.introduction} {this.props.greeting}</h1>
-                <button>{this.state.buttonText}</button>
+                <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
             </div>
         )
     }
